b10e59be5e46a3351f34eca25fa75645509163bf
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-11-14 11:13:17 +0200

    Added all eidas custom types.

--- src/main/java/gr/grnet/keycloak/idp/parsers/EidasSAMLAttributeValueParser.java
+++ src/main/java/gr/grnet/keycloak/idp/parsers/EidasSAMLAttributeValueParser.java
@@ -2,8 +2,9 @@ package gr.grnet.keycloak.idp.parsers;
 
 import java.io.StringWriter;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
@@ -29,114 +30,133 @@ import org.keycloak.saml.processing.core.saml.v2.util.XMLTimeUtil;
  */
 public class EidasSAMLAttributeValueParser implements StaxParser {
 
-    private static final PicketLinkLogger logger = PicketLinkLoggerFactory.getLogger();
+	private static final PicketLinkLogger logger = PicketLinkLoggerFactory.getLogger();
 
-    private static final EidasSAMLAttributeValueParser INSTANCE = new EidasSAMLAttributeValueParser();
-    private static final QName NIL = new QName(JBossSAMLURIConstants.XSI_NSURI.get(), "nil", JBossSAMLURIConstants.XSI_PREFIX.get());
-    private static final QName XSI_TYPE = new QName(JBossSAMLURIConstants.XSI_NSURI.get(), "type", JBossSAMLURIConstants.XSI_PREFIX.get());
+	private static final EidasSAMLAttributeValueParser INSTANCE = new EidasSAMLAttributeValueParser();
+	private static final QName NIL = new QName(JBossSAMLURIConstants.XSI_NSURI.get(), "nil",
+			JBossSAMLURIConstants.XSI_PREFIX.get());
+	private static final QName XSI_TYPE = new QName(JBossSAMLURIConstants.XSI_NSURI.get(), "type",
+			JBossSAMLURIConstants.XSI_PREFIX.get());
 
-    private static final List<String> EIDAS_TYPES = Arrays.asList(":PersonIdentifierType", ":CurrentFamilyNameType", ":CurrentGivenNameType", ":DateOfBirthType");
-    
-    public static EidasSAMLAttributeValueParser getInstance() {
-        return INSTANCE;
-    }
+	private static final Set<String> EIDAS_TYPES_STRING = new HashSet<>(
+			Arrays.asList("PersonIdentifierType", "CurrentAddressType", "GenderType", "PlaceOfBirthType",
+					"LegalPersonIdentifierType", "LegalPersonAddressType", "VATRegistrationNumberType",
+					"TaxReferenceType", "D-2012-17-EUIdentifierType", "LEIType", "EORIType", "SEEDType",
+					"SICType", "CurrentGivenNameType", "CurrentFamilyNameType", "BirthNameType", "LegalNameType"));
+	private static final Set<String> EIDAS_TYPES_DATE = new HashSet<>(Arrays.asList("DateOfBirthType"));
+	private static final Set<String> EIDAS_TYPES_ANYTYPE = new HashSet<>(
+			Arrays.asList("CurrentAddressStructuredType", "LegalPersonAddressStructuredType"));
 
-    @Override
-    public Object parse(XMLEventReader xmlEventReader) throws ParsingException {
-        StartElement element = StaxParserUtil.getNextStartElement(xmlEventReader);
-        StaxParserUtil.validate(element, SAMLAssertionQNames.ATTRIBUTE_VALUE.getQName());
+	public static EidasSAMLAttributeValueParser getInstance() {
+		return INSTANCE;
+	}
 
-        Attribute nil = element.getAttributeByName(NIL);
-        if (nil != null) {
-            String nilValue = StaxParserUtil.getAttributeValue(nil);
-            if (nilValue != null && (nilValue.equalsIgnoreCase("true") || nilValue.equals("1"))) {
-                String elementText = StaxParserUtil.getElementText(xmlEventReader);
-                if (elementText == null || elementText.isEmpty()) {
-                    return null;
-                } else {
-                    throw logger.nullValueError("nil attribute is not in SAML20 format");
-                }
-            } else {
-                throw logger.parserRequiredAttribute(JBossSAMLURIConstants.XSI_PREFIX.get() + ":nil");
-            }
-        }
+	@Override
+	public Object parse(XMLEventReader xmlEventReader) throws ParsingException {
+		StartElement element = StaxParserUtil.getNextStartElement(xmlEventReader);
+		StaxParserUtil.validate(element, SAMLAssertionQNames.ATTRIBUTE_VALUE.getQName());
 
-        Attribute type = element.getAttributeByName(XSI_TYPE);
-        if (type == null) {
-            if (StaxParserUtil.hasTextAhead(xmlEventReader)) {
-                return StaxParserUtil.getElementText(xmlEventReader);
-            }
-            // Else we may have Child Element
-            XMLEvent xmlEvent = StaxParserUtil.peek(xmlEventReader);
-            if (xmlEvent instanceof StartElement) {
-                element = (StartElement) xmlEvent;
-                final QName qName = element.getName();
-                if (Objects.equals(qName, SAMLAssertionQNames.NAMEID.getQName())) {
-                    return SAMLParserUtil.parseNameIDType(xmlEventReader);
-                }
-            } else if (xmlEvent instanceof EndElement) {
-                return "";
-            }
+		Attribute nil = element.getAttributeByName(NIL);
+		if (nil != null) {
+			String nilValue = StaxParserUtil.getAttributeValue(nil);
+			if (nilValue != null && (nilValue.equalsIgnoreCase("true") || nilValue.equals("1"))) {
+				String elementText = StaxParserUtil.getElementText(xmlEventReader);
+				if (elementText == null || elementText.isEmpty()) {
+					return null;
+				} else {
+					throw logger.nullValueError("nil attribute is not in SAML20 format");
+				}
+			} else {
+				throw logger.parserRequiredAttribute(JBossSAMLURIConstants.XSI_PREFIX.get() + ":nil");
+			}
+		}
 
-            // when no type attribute assigned -> assume anyType
-            return parseAnyTypeAsString(xmlEventReader);
-        }
+		Attribute type = element.getAttributeByName(XSI_TYPE);
+		if (type == null) {
+			if (StaxParserUtil.hasTextAhead(xmlEventReader)) {
+				return StaxParserUtil.getElementText(xmlEventReader);
+			}
+			// Else we may have Child Element
+			XMLEvent xmlEvent = StaxParserUtil.peek(xmlEventReader);
+			if (xmlEvent instanceof StartElement) {
+				element = (StartElement) xmlEvent;
+				final QName qName = element.getName();
+				if (Objects.equals(qName, SAMLAssertionQNames.NAMEID.getQName())) {
+					return SAMLParserUtil.parseNameIDType(xmlEventReader);
+				}
+			} else if (xmlEvent instanceof EndElement) {
+				return "";
+			}
 
-        //      RK Added an additional type check for base64Binary type as calheers is passing this type
-        String typeValue = StaxParserUtil.getAttributeValue(type);
-        if (typeValue.contains(":string")) {
-            return StaxParserUtil.getElementText(xmlEventReader);
-        } else if (typeValue.contains(":anyType")) {
-            return parseAnyTypeAsString(xmlEventReader);
-        } else if(typeValue.contains(":base64Binary")){
-            return StaxParserUtil.getElementText(xmlEventReader);
-        } else if(typeValue.contains(":date")){
-            return XMLTimeUtil.parse(StaxParserUtil.getElementText(xmlEventReader));
-        } else if(typeValue.contains(":boolean")){
-            return StaxParserUtil.getElementText(xmlEventReader);
-        } else {
-        	// FIXME: check type of each additional type
-        	for(String eidasType: EIDAS_TYPES) { 
-        		if (typeValue.contains(eidasType)) { 
-        			return StaxParserUtil.getElementText(xmlEventReader);
-        		}
-        	}
-        }
-        
-        // KEYCLOAK-18417: Simply ignore unknown types
-        logger.debug("Skipping attribute value of unsupported type " + typeValue);
-        StaxParserUtil.bypassElementBlock(xmlEventReader);
-        return null;
-    }
+			// when no type attribute assigned -> assume anyType
+			return parseAnyTypeAsString(xmlEventReader);
+		}
 
-    public static String parseAnyTypeAsString(XMLEventReader xmlEventReader) throws ParsingException {
-        try {
-            XMLEvent event = xmlEventReader.peek();
-            if (event.isStartElement()) {
-                event = xmlEventReader.nextTag();
-                StringWriter sw = new StringWriter();
-                XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(sw);
-                //QName tagName = event.asStartElement().getName();
-                int tagLevel = 1;
-                do {
-                    writer.add(event);
-                    event = (XMLEvent) xmlEventReader.next();
-                    if (event.isStartElement()) {
-                        tagLevel++;
-                    }
-                    if (event.isEndElement()) {
-                        tagLevel--;
-                    }
-                } while (xmlEventReader.hasNext() && tagLevel > 0);
-                writer.add(event);
-                writer.flush();
-                return sw.toString();
-            } else {
-                return StaxParserUtil.getElementText(xmlEventReader);
-            }
-        } catch (Exception e) {
-            throw logger.parserError(e);
-        }
-    }
+		// RK Added an additional type check for base64Binary type as calheers is
+		// passing this type
+		String typeValue = StaxParserUtil.getAttributeValue(type);
+		if (typeValue.contains(":string")) {
+			return StaxParserUtil.getElementText(xmlEventReader);
+		} else if (typeValue.contains(":anyType")) {
+			return parseAnyTypeAsString(xmlEventReader);
+		} else if (typeValue.contains(":base64Binary")) {
+			return StaxParserUtil.getElementText(xmlEventReader);
+		} else if (typeValue.contains(":date")) {
+			return XMLTimeUtil.parse(StaxParserUtil.getElementText(xmlEventReader));
+		} else if (typeValue.contains(":boolean")) {
+			return StaxParserUtil.getElementText(xmlEventReader);
+		} else {
+			// eIDAS specific types
+			String[] typeValueParts = typeValue.split(":");
+			if (typeValueParts.length >= 2) {
+				String typeValuePart = typeValueParts[1];
+				if (EIDAS_TYPES_STRING.contains(typeValuePart)) {
+					logger.debug("Handling eIDAS specific type " + typeValuePart + " as string");
+					return StaxParserUtil.getElementText(xmlEventReader);
+				} else if (EIDAS_TYPES_DATE.contains(typeValuePart)) {
+					logger.debug("Handling eIDAS specific type " + typeValuePart + " as date");
+					return XMLTimeUtil.parse(StaxParserUtil.getElementText(xmlEventReader));
+				} else if (EIDAS_TYPES_ANYTYPE.contains(typeValuePart)) { 
+					logger.debug("Handling eIDAS specific type " + typeValuePart + " as any time");
+					return parseAnyTypeAsString(xmlEventReader);
+				}
+			}
+		}
+
+		// KEYCLOAK-18417: Simply ignore unknown types
+		logger.debug("Skipping attribute value of unsupported type " + typeValue);
+		StaxParserUtil.bypassElementBlock(xmlEventReader);
+		return null;
+	}
+
+	public static String parseAnyTypeAsString(XMLEventReader xmlEventReader) throws ParsingException {
+		try {
+			XMLEvent event = xmlEventReader.peek();
+			if (event.isStartElement()) {
+				event = xmlEventReader.nextTag();
+				StringWriter sw = new StringWriter();
+				XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(sw);
+				// QName tagName = event.asStartElement().getName();
+				int tagLevel = 1;
+				do {
+					writer.add(event);
+					event = (XMLEvent) xmlEventReader.next();
+					if (event.isStartElement()) {
+						tagLevel++;
+					}
+					if (event.isEndElement()) {
+						tagLevel--;
+					}
+				} while (xmlEventReader.hasNext() && tagLevel > 0);
+				writer.add(event);
+				writer.flush();
+				return sw.toString();
+			} else {
+				return StaxParserUtil.getElementText(xmlEventReader);
+			}
+		} catch (Exception e) {
+			throw logger.parserError(e);
+		}
+	}
 
 }
