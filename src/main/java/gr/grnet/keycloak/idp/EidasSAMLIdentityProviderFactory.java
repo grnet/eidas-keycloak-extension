3b842d7ac3923a1c1c7dbfc1ca3b85d6fd3834b3
Dimitrios Michail <dimitrios.michail@gmail.com>
2024-05-20 19:19:44 +0300

    Added method with default implementation for SAML parseConfig.

--- src/main/java/gr/grnet/keycloak/idp/EidasSAMLIdentityProviderFactory.java
+++ src/main/java/gr/grnet/keycloak/idp/EidasSAMLIdentityProviderFactory.java
@@ -16,21 +16,40 @@
 package gr.grnet.keycloak.idp;
 
 import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.namespace.QName;
+
 import org.keycloak.Config.Scope;
 import org.keycloak.broker.provider.IdentityProviderFactory;
+import org.keycloak.broker.saml.SAMLIdentityProviderConfig;
+import org.keycloak.dom.saml.v2.assertion.AttributeType;
+import org.keycloak.dom.saml.v2.metadata.EndpointType;
+import org.keycloak.dom.saml.v2.metadata.EntityDescriptorType;
+import org.keycloak.dom.saml.v2.metadata.IDPSSODescriptorType;
+import org.keycloak.dom.saml.v2.metadata.KeyDescriptorType;
+import org.keycloak.dom.saml.v2.metadata.KeyTypes;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
+import org.keycloak.saml.common.constants.JBossSAMLURIConstants;
+import org.keycloak.saml.common.exceptions.ParsingException;
+import org.keycloak.saml.common.util.DocumentUtil;
+import org.keycloak.saml.processing.core.saml.v2.util.SAMLMetadataUtil;
 import org.keycloak.saml.validators.DestinationValidator;
+import org.w3c.dom.Element;
 
 public class EidasSAMLIdentityProviderFactory implements IdentityProviderFactory<EidasSAMLIdentityProvider> {
 
 	public static final String PROVIDER_ID = "eidas-saml";
+
+    private static final String MACEDIR_ENTITY_CATEGORY = "http://macedir.org/entity-category";
+    private static final String REFEDS_HIDE_FROM_DISCOVERY = "http://refeds.org/category/hide-from-discovery";
 
 	private DestinationValidator destinationValidator;
 
@@ -102,9 +121,103 @@ public class EidasSAMLIdentityProviderFactory implements IdentityProviderFactory
 	public void close() {
 	}
 
-	@Override
-	public Map<String, String> parseConfig(KeycloakSession session, InputStream inputStream) {
-		throw new UnsupportedOperationException("Unimplemented method 'parseConfig'");
-	}
+    @Override
+    public Map<String, String> parseConfig(KeycloakSession session, InputStream inputStream) {
+        try {
+            EntityDescriptorType entityType = SAMLMetadataUtil.parseEntityDescriptorType(inputStream);
+            IDPSSODescriptorType idpDescriptor = SAMLMetadataUtil.locateIDPSSODescriptorType(entityType);
+
+            if (idpDescriptor != null) {
+                SAMLIdentityProviderConfig samlIdentityProviderConfig = new SAMLIdentityProviderConfig();
+                String singleSignOnServiceUrl = null;
+                boolean postBindingResponse = false;
+                boolean postBindingLogout = false;
+                for (EndpointType endpoint : idpDescriptor.getSingleSignOnService()) {
+                    if (endpoint.getBinding().toString().equals(JBossSAMLURIConstants.SAML_HTTP_POST_BINDING.get())) {
+                        singleSignOnServiceUrl = endpoint.getLocation().toString();
+                        postBindingResponse = true;
+                        break;
+                    } else if (endpoint.getBinding().toString().equals(JBossSAMLURIConstants.SAML_HTTP_REDIRECT_BINDING.get())) {
+                        singleSignOnServiceUrl = endpoint.getLocation().toString();
+                    }
+                }
+                String singleLogoutServiceUrl = null;
+                for (EndpointType endpoint : idpDescriptor.getSingleLogoutService()) {
+                    if (postBindingResponse && endpoint.getBinding().toString().equals(JBossSAMLURIConstants.SAML_HTTP_POST_BINDING.get())) {
+                        singleLogoutServiceUrl = endpoint.getLocation().toString();
+                        postBindingLogout = true;
+                        break;
+                    } else if (!postBindingResponse && endpoint.getBinding().toString().equals(JBossSAMLURIConstants.SAML_HTTP_REDIRECT_BINDING.get())) {
+                        singleLogoutServiceUrl = endpoint.getLocation().toString();
+                        break;
+                    }
+
+                }
+                samlIdentityProviderConfig.setIdpEntityId(entityType.getEntityID());
+                samlIdentityProviderConfig.setSingleLogoutServiceUrl(singleLogoutServiceUrl);
+                samlIdentityProviderConfig.setSingleSignOnServiceUrl(singleSignOnServiceUrl);
+                samlIdentityProviderConfig.setWantAuthnRequestsSigned(idpDescriptor.isWantAuthnRequestsSigned());
+                samlIdentityProviderConfig.setAddExtensionsElementWithKeyInfo(false);
+                samlIdentityProviderConfig.setValidateSignature(idpDescriptor.isWantAuthnRequestsSigned());
+                samlIdentityProviderConfig.setPostBindingResponse(postBindingResponse);
+                samlIdentityProviderConfig.setPostBindingAuthnRequest(postBindingResponse);
+                samlIdentityProviderConfig.setPostBindingLogout(postBindingLogout);
+                samlIdentityProviderConfig.setLoginHint(false);
+
+                List<String> nameIdFormatList = idpDescriptor.getNameIDFormat();
+                if (nameIdFormatList != null && !nameIdFormatList.isEmpty()) {
+                    samlIdentityProviderConfig.setNameIDPolicyFormat(nameIdFormatList.get(0));
+                }
+
+                List<KeyDescriptorType> keyDescriptor = idpDescriptor.getKeyDescriptor();
+                String defaultCertificate = null;
+
+                if (keyDescriptor != null) {
+                    for (KeyDescriptorType keyDescriptorType : keyDescriptor) {
+                        Element keyInfo = keyDescriptorType.getKeyInfo();
+                        Element x509KeyInfo = DocumentUtil.getChildElement(keyInfo, new QName("dsig", "X509Certificate"));
+
+                        if (KeyTypes.SIGNING.equals(keyDescriptorType.getUse())) {
+                            samlIdentityProviderConfig.addSigningCertificate(x509KeyInfo.getTextContent());
+                        } else if (KeyTypes.ENCRYPTION.equals(keyDescriptorType.getUse())) {
+                            samlIdentityProviderConfig.setEncryptionPublicKey(x509KeyInfo.getTextContent());
+                        } else if (keyDescriptorType.getUse() == null) {
+                            defaultCertificate = x509KeyInfo.getTextContent();
+                        }
+                    }
+                }
+
+                if (defaultCertificate != null) {
+                    if (samlIdentityProviderConfig.getSigningCertificates().length == 0) {
+                        samlIdentityProviderConfig.addSigningCertificate(defaultCertificate);
+                    }
+
+                    if (samlIdentityProviderConfig.getEncryptionPublicKey() == null) {
+                        samlIdentityProviderConfig.setEncryptionPublicKey(defaultCertificate);
+                    }
+                }
+
+                samlIdentityProviderConfig.setEnabledFromMetadata(entityType.getValidUntil() == null
+                        || entityType.getValidUntil().toGregorianCalendar().getTime().after(new Date(System.currentTimeMillis())));
+
+                // check for hide on login attribute
+                if (entityType.getExtensions() != null && entityType.getExtensions().getEntityAttributes() != null) {
+                    for (AttributeType attribute : entityType.getExtensions().getEntityAttributes().getAttribute()) {
+                        if (MACEDIR_ENTITY_CATEGORY.equals(attribute.getName())
+                                && attribute.getAttributeValue().contains(REFEDS_HIDE_FROM_DISCOVERY)) {
+                            samlIdentityProviderConfig.setHideOnLogin(true);
+                        }
+                    }
+
+                }
+
+                return samlIdentityProviderConfig.getConfig();
+            }
+        } catch (ParsingException pe) {
+            throw new RuntimeException("Could not parse IdP SAML Metadata", pe);
+        }
+
+        return new HashMap<>();
+    }
 
 }
